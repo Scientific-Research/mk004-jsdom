1f4f7de2ede6ddf54d2fc3b2e56a3dd678cd5130
Maximilian <scientificresearch309@gmail.com>
2025-08-22 23:38:25 +0200

    some DOM manipulations

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log('javascript works!');
+
+const messageElem = document.querySelector('.message');
+
+console.log(messageElem.innerText);
+console.log(messageElem.innerHTML);
+
+messageElem.innerText = "Hallo, wie geht's";
+console.log(messageElem);
