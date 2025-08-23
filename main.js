1cbccd3def55d0e1eac0fa3c1581c11b4a8bf16f
Maximilian <scientificresearch309@gmail.com>
2025-08-23 10:16:16 +0200

    button click chnage the page

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ const btnElem = document.querySelector('.btn');
 messageElem.innerText = 'Initial Text';
 
 btnElem.addEventListener('click', () => {
-  messageElem.innerText = 'Text button was clicked!';
+  messageElem.innerText = 'The button was clicked!';
+  // messageElem.innerHTML = 'the <b>button</b> was clicked!';
 });
