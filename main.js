96d766fc103686095c409bda1530714cd27d2b87
Maximilian <scientificresearch309@gmail.com>
2025-08-23 14:24:34 +0200

    Eingabefeld funktioniert

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const messageElem = document.querySelector('.message');
 const btnElem = document.querySelector('.btn');
+const inputElem = document.querySelector('.userText');
 
 messageElem.innerText = 'Initial Text';
 
 // Event mit addEventListener
-btnElem.addEventListener('click', () => {
-  messageElem.innerText = 'The button was clicked!';
-  // messageElem.innerHTML = 'the <b>button</b> was clicked!';
-});
+// btnElem.addEventListener('click', () => {
+//   messageElem.innerText = 'The button was clicked!';
+//   // messageElem.innerHTML = 'the <b>button</b> was clicked!';
+// });
 
 // Event mit Onclick()
 // const test = () => {
@@ -17,3 +18,23 @@ btnElem.addEventListener('click', () => {
 // const changeText = () => {
 //   messageElem.innerText = 'The Button was clicked!!!!!';
 // };
+
+// implement onkeydown
+btnElem.addEventListener('click', () => {
+  // messageElem.innerText = 'The button was clicked!';
+  // messageElem.innerHTML = 'the <b>button</b> was clicked!';
+  // console.log(inputElem);
+  console.log(inputElem.value);
+  if (inputElem.value !== '') {
+    messageElem.innerText = inputElem.value;
+  }
+
+  // else {
+  //   return;
+  // }
+
+  // if (inputElem.value === '') {
+  //   messageElem.innerText = 'Initial Text';
+  // }
+  inputElem.value = '';
+});
