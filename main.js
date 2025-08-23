e9236cf2a3712c6da8f5ac009d5ba2d87f03517d
Maximilian <scientificresearch309@gmail.com>
2025-08-23 13:47:42 +0200

    zurück zur addEventListener => Separaton of Concerns

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ const btnElem = document.querySelector('.btn');
 messageElem.innerText = 'Initial Text';
 
 // Event mit addEventListener
-// btnElem.addEventListener('click', () => {
-//   messageElem.innerText = 'The button was clicked!';
-//   // messageElem.innerHTML = 'the <b>button</b> was clicked!';
-// });
+btnElem.addEventListener('click', () => {
+  messageElem.innerText = 'The button was clicked!';
+  // messageElem.innerHTML = 'the <b>button</b> was clicked!';
+});
 
 // Event mit Onclick()
 // const test = () => {
 //   messageElem.innerText = 'The Button was clicked!!!!!';
 // };
 
-const changeText = () => {
-  messageElem.innerText = 'The Button was clicked!!!!!';
-};
+// const changeText = () => {
+//   messageElem.innerText = 'The Button was clicked!!!!!';
+// };
